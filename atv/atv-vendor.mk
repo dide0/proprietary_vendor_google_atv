a8a520b85002f124a62ec1a8942fa03522c6fc63
Matt Gorski <matt.gorski@gmail.com>
2015-11-20 00:49:45 -0500

    Update atv-vendor.mk
    Molly test build

--- atv/atv-vendor.mk
+++ atv/atv-vendor.mk
@@ -30,8 +30,8 @@ PRODUCT_PACKAGES += \
     YouTubeLeanback \
 
 PRODUCT_COPY_FILES += \
-    vendor/google/atv/misc/com.google.android.media.effects.jar:system/framework/com.google.android.media.effects.jar \
-    vendor/google/atv/misc/com.google.widevine.software.drm.jar:system/framework/com.google.widevine.software.drm.jar: \
-    vendor/google/atv/misc/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
-    vendor/google/atv/misc/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml
+    vendor/google/molly/misc/com.google.android.media.effects.jar:system/framework/com.google.android.media.effects.jar \
+    vendor/google/molly/misc/com.google.widevine.software.drm.jar:system/framework/com.google.widevine.software.drm.jar: \
+    vendor/google/molly/misc/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
+    vendor/google/molly/misc/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml
 
