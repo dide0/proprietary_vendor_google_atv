edb445bbc6aa856c7275cd9ec8dcb9d458b7a342
Matthias <carpentier.matthias@gmail.com>
2017-12-30 16:37:31 +0100

    added extra apps

--- atv/atv-vendor.mk
+++ atv/atv-vendor.mk
@@ -22,11 +22,13 @@ PRODUCT_PACKAGES += \
 
 
 PRODUCT_COPY_FILES += \
+    vendor/google/atv/app/GamepadPairingService.apk:system/priv-app/GamepadPairingService/GamepadPairingService.apk \
+    vendor/google/atv/app/ConfigUpdater.apk:system/app/ConfigUpdater/ConfigUpdater.apk \
     vendor/google/atv/app/GoogleTTS.apk:system/app/GoogleTTS/GoogleTTS.apk \
     vendor/google/atv/app/WebViewGoogle.apk:system/app/WebViewGoogle/WebViewGoogle.apk \
     vendor/google/atv/app/Katniss.apk:system/priv-app/Katniss/Katniss.apk \
     vendor/google/atv/app/GoogleBackupTransport.apk:system/priv-app/GoogleBackupTransport/GoogleBackupTransport.apk \
     vendor/google/atv/lib/libwebviewchromium.so:system/app/WebViewGoogle/lib/arm/libwebviewchromium.so \
-    vendor/google/atv/lib/libgoogle_hotword_jni.so:system/priv-app/katniss/lib/arm/libgoogle_hotword_jni.so \
+    vendor/google/atv/lib/libgoogle_hotword_jni.so:system/priv-app/Katniss/lib/arm/libgoogle_hotword_jni.so \
     vendor/google/atv/lib/libgoogle_recognizer_jni_l.so:system/priv-app/Katniss/lib/arm/libgoogle_recognizer_jni_l.so \
-    vendor/google/atv/lib/libvision_face_jni.so:system/priv-app/katniss/lib/arm/libvision_face_jni.so 
+    vendor/google/atv/lib/libvision_face_jni.so:system/priv-app/Katniss/lib/arm/libvision_face_jni.so
